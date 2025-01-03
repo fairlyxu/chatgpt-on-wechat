5f86e2fb01efd024958a8dcee4484a50490c385a
emilyyufen <675520238@qq.com>
2025-01-03 18:58:48 +0800

    bug修复

--- appDesktopMain.py
+++ appDesktopMain.py
@@ -34,7 +34,7 @@ class API:
     def loadConfig(self, tenantId):
         self.config_file = 'config_' + tenantId + '.json'
         # 判断文件是否存在，如不存在，则加载config-template.json默认配置文件
-        if not os.path.exists(self.config_file):
+        if not os.path.exists(self.config_file): 
             self.config_file = 'config-template.json'
         if not os.path.exists(self.config_file):  # 如默认配置文件不存在，则报错
             raise Exception('配置文件不存在，且无默认配置文件')
@@ -54,6 +54,11 @@ class API:
 
     def startChat(self, configObj="", tenantId="", token=""):
         if not self.is_running():
+            # 调用saveConfig保存配置文件
+            self.saveConfig(configObj, tenantId, token)
+            filename = 'config_' + tenantId + '.json'
+            if not os.path.exists(filename):  # 如默认配置文件不存在，则报错
+                raise Exception('配置文件不存在')
             print(f"Starting {self.script_name} with config file: {self.config_file}")
             args = [sys.executable, self.script_name]
             if self.config_file:
